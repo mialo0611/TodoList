8c197d38dac020c5954cf90aac2759ba75fd2239
MiaL <liaomia@gmail.com>
2026-06-11 16:51:25 +0800

    第一個版本

---
+++ errorHandler.js
@@ -0,0 +1,16 @@
+function errorHandler(rsp,message){
+    const headers = {
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
+        'Content-Type': 'application/json'
+    }; 
+    rsp.writeHead(400, headers);
+    rsp.write(JSON.stringify({
+                "status": "failed",
+                "message": message
+        }));
+    rsp.end();
+}
+
+module.exports = errorHandler;
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const http = require("http");
 const { v4: uuidv4 } = require("uuid");
-const  id = uuidv4();
+//這裏的id 在網頁未重新post之前會一直是同一個，會導致新增的待辦事項id相同，所以要放在requestListener裡面，每次post請求都會產生新的id
+//const  id = uuidv4(); 
+const errHandler = require('./errorHandler');
 const todos =[];
 
 const requestListener = (req, rsp) => {
@@ -11,9 +13,10 @@ const requestListener = (req, rsp) => {
         'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
         'Content-Type': 'application/json'
     };  
-    let body = '';
-    //req.setEncoding('utf-8');
 
+    let body = '';
+    req.setEncoding('utf-8');
+    //get request body data
     req.on('data', chunk => {
         //console.log(chunk);
         body += chunk;
@@ -28,31 +31,87 @@ const requestListener = (req, rsp) => {
         rsp.end();
     } else if (req.url === '/todos' && req.method === 'POST'){
         req.on('end', () => {
-            const title = JSON.parse(body).title; 
-            const newTodo = {
-                id: id,
-                title: title
-            };
-            todos.push(newTodo);
+            try{
+                const title = JSON.parse(body).title; 
+                if (title !== undefined){
+                    const  id = uuidv4();
+                    const newTodo = {
+                        id: id,
+                        title: title
+                    };
+                    todos.push(newTodo);
+                    rsp.writeHead(200, headers);
+                    rsp.write(JSON.stringify({
+                        "status": "success",
+                        "data": todos
+                    }));
+                    rsp.end();
+                }else{
+                    errHandler(rsp, "傳入內容格式錯誤");
+                }                    
+            }catch(err){
+                errHandler(rsp, err.message);                 
+            }            
+        }); 
+    }else if (req.method === 'DELETE'){
+        if (req.url.startsWith('/todos/')){
+            const id = req.url.split('/').pop();
+            //console.log(id);
+            const idx = todos.findIndex(todo => todo.id === id);
+            if (idx !== -1){
+                todos.splice(idx, 1);
+                rsp.writeHead(200, headers);                        
+                rsp.write(JSON.stringify({
+                        "status": "success",
+                        "data": todos
+                }));
+                rsp.end();
+            } else {
+                errHandler(rsp, "找不到該待辦事項");
+            }    
+        } else if(req.url === '/todos'){
+            todos.length = 0;
             rsp.writeHead(200, headers);
             rsp.write(JSON.stringify({
                 "status": "success",
                 "data": todos
             }));
-            rsp.end();
-        });
-               
+            rsp.end();        
+        } else {
+            errHandler(rsp, "格式有誤，請確是否正確");
+        }
+    }else if (req.url.startsWith('/todos/') && req.method === 'PATCH'){  
+        req.on('end', () => {
+            try{
+                const titleUpd = JSON.parse(body).title;
+                if (titleUpd !== undefined){
+                    const id = req.url.split('/').pop();
+                    //console.log(id);
+                    const idx = todos.findIndex(todo => todo.id === id);
+                    if (idx !== -1){
+                        todos[idx].title = titleUpd;
+                        rsp.writeHead(200, headers);                        
+                        rsp.write(JSON.stringify({
+                                "status": "success",
+                                "data": todos
+                        }));
+                        rsp.end();
+                    }else{
+                        errHandler(rsp, "找不到該待辦事項");
+                    }
+                }else{
+                    errHandler(rsp, "傳入內容格式錯誤");
+                }                    
+            }catch(err){
+                errHandler(rsp, err.message);                 
+            }            
+        });         
     } else if (req.url === '/todos' && req.method === 'OPTIONS'){
         rsp.writeHead(200, headers);
         rsp.end();
     }else{
-        rsp.writeHead(404, headers);
-        rsp.write(JSON.stringify({
-            "status" :"failed",
-            "message": 'Not Found'
-        }));
-        rsp.end();
+        errHandler(rsp, "Not Found");        
     }
 }
 const server = http.createServer(requestListener);
-server.listen(3030);
+server.listen(process.env.PORT || 8080);
